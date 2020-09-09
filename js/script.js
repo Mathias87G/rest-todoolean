ddbc50e89aa541d8bc97510d6adb90c09f20b528
Mathias <mathias.grandin@gmail.com>
2020-09-09 15:56:42 +0200

    editdata da sistemare

---
+++ js/script.js
@@ -0,0 +1,117 @@
+$(document).ready(function(){
+
+  getData();
+
+  $(document).on('click', 'span.delete', function(){
+    var elemento = $(this);
+    var idToDo = elemento.parent().attr('data-id');
+    deleteElement(idToDo);
+  });
+
+  $(document).on('click', '.list', function(){
+    var oggetto = $(this);
+    var elemento = $(this).text();
+    $('input.edit').addClass('hidden');
+    $(this).children('input').removeClass('hidden').val(elemento);
+  });
+
+  $('.inserisci').click(function(){
+    var newElement = $('#nuova-voce').val();
+    createElement(newElement);
+  });
+
+  $('.edit').keydown(keyboard);
+});
+
+function createElement(elemento){
+  $.ajax(
+    {
+      url: 'http://157.230.17.132:3013/todos',
+      method: 'POST',
+      data: {
+        text: elemento
+      },
+      success: function(risposta){
+        $('.todos').html('');
+        getData();
+      },
+      error: function(){
+        alert('Errore!')
+      }
+    }
+  );
+}
+
+function deleteElement(id){
+  $.ajax(
+    {
+      url: 'http://157.230.17.132:3013/todos/' + id,
+      method: 'DELETE',
+      success: function(risposta){
+        $('.todos').html('');
+        getData();
+      },
+      error: function(){
+        alert('Errore!')
+      }
+    }
+  );
+}
+
+function getData(){
+  $.ajax(
+    {
+      url: 'http://157.230.17.132:3013/todos',
+      method: 'GET',
+      success: function(risposta){
+        getElement(risposta);
+      },
+      error: function(){
+        alert('Errore!');
+      }
+    }
+  );
+}
+
+function getElement(data){
+  var source = $('#entry-template').html();
+  var template = Handlebars.compile(source);
+
+  for (var i = 0; i < data.length; i++){
+    var context = {
+      text: data[i].text,
+      id: data[i].id
+    }
+    var html = template(context);
+    $('.todos').append(html);
+  }
+}
+
+function keyboard(){
+  if (event.which == 13 || event.keyCode == 13) {
+    var elemento = $(this);
+    var idToDo = elemento.parent().attr('data-id');
+    editData(idToDo);
+  }
+}
+
+function editData(id){
+  var edit = $('.item[data-id="' + id + '"]').find('input').val();
+
+  $.ajax(
+    {
+      url: 'http://157.230.17.132:3013/todos/' + id,
+      method: 'PUT',
+      data: {
+        text: edit
+      },
+      success: function(data){
+        $('.todos').html('');
+        getData();
+      },
+      error: function(){
+        alert('Errore!');
+      }
+    }
+  );
+}
